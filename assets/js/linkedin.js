365faf41ade85fc2c6b4b4f736b3179f337cfaf3
Essa Khan <eskhansa@gmail.com>
2021-06-01 10:25:05 -0500

    update linkedin script

---
+++ assets/js/linkedin.js
@@ -0,0 +1,2 @@
+document.write( '<script src=\"https://platform.linkedin.com/badges/js/profile.js\" async defer type=\"text/javascript\"></script>\n' );
+document.write( '<div class=\"badge-base LI-profile-badge\" data-locale=\"en_US\" data-size=\"large\" data-theme=\"dark\" data-type=\"HORIZONTAL\" data-vanity=\"essa-khan\" data-version=\"v1\" data-position=\"center center\"><a class=\"badge-base__link LI-simple-link\" href=\"https://www.linkedin.com/in/essa-khan?trk=profile-badge\"></a></div><br />' );
